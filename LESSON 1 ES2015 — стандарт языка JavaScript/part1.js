b5bc029becd7959989450f0cca2adc305017f9bb
taranse <bboy.stenli@yandex.ru>
2016-11-05 23:44:28 +0300

    Update part1.js

--- LESSON 1 ES2015 — стандарт языка JavaScript/part1.js
+++ LESSON 1 ES2015 — стандарт языка JavaScript/part1.js
@@ -1,39 +1,41 @@
-class PokemonList {
-  constructor() {
-    this.nameList = list;
-  }
-  add(newPokemon, level, ability, fraction){
-    this.nameList[newPokemon.name] = newPokemon;
-    this.nameList[newPokemon.name].level = level;
-    this.nameList[newPokemon.name].ability = {};
-    this.nameList[newPokemon.name].ability.strenght = ability[0];
-    this.nameList[newPokemon.name].ability.agility = ability[1];
-    this.nameList[newPokemon.name].ability.intelligence = ability[2];
-    this.nameList[newPokemon.name].fraction = fraction;
-  }
-  showFraction(fraction){
-    var mass = [];
-    for(let names in this.nameList){
-      if(this.nameList[names].fraction == fraction)
-        mass.push(this.nameList[names].name)
+class PokemonList extends Array{
+  constructor(...list) {
+    super(array);
+    for (var name in list){
+      this[name] = list[name];
     }
-    console.log(`Покемоны из фракции ${fraction} - ${mass}`);
   }
+  add(...list) {
+    this.push({name:list[0],level:list[1],ability:list[2],fraction:list[3]})
+  }
+  showList(){
+    let mass = [];
+    for(var name in this)
+      mass.push(`\n${this[name].name} - ${this[name].level}. ${this[name].fraction}`);
+    console.log(`${mass}`)
+  }
+  // showFraction(fraction) {
+  //   let mass = [];
+  //   for (let names in this.nameList) {
+  //     if (this.nameList[names].fraction == fraction)
+  //       mass.push(this.nameList[names].name)
+  //   }
+  //   console.log(`Покемоны из фракции ${fraction} - ${mass}`);
+  // }
 }
 
-class Pokemon extends PokemonList{
-  constructor(nameList, name){
-    super(nameList);
-    this.name = nameList.name;
-    this.level = nameList.level;
-    this.fraction = nameList.fraction;
+class Pokemon {
+  constructor(name) {
+    this.name = name.name;
+    this.level = name.level;
+    this.fraction = name.fraction;
     this.ability = [
-      nameList.ability.strenght, 
-      nameList.ability.agility, 
-      nameList.ability.intelligence
+      name.ability.strenght, 
+      name.ability.agility, 
+      name.ability.intelligence
     ];
   }
-  show(){
+  show() {
     console.log(`
       Покемон: ${this.name}, 
       Уровень: ${this.level}, 
@@ -41,36 +43,39 @@ class Pokemon extends PokemonList{
       Фракция: ${this.fraction}
     `);
   }
-  set levelUp(level){
+  set levelUp(level) {
     this.level += 1;
   }
 }
-
 var Pickachu = {
   name: 'Pickachu',
-  level : 13,
-  ability : {
-    strenght : 15,
-    agility : 18,
-    intelligence : 22
+  level: 13,
+  ability: {
+    strenght: 15,
+    agility: 18,
+    intelligence: 22
   },
-  fraction : 'fire'
+  fraction: 'fire'
 }
 var Bulbasaur = {
   name: 'Bulbasaur',
-  level : 21,
-  ability : {
-    strenght : 16,
-    agility : 25,
-    intelligence : 11
+  level: 21,
+  ability: {
+    strenght: 16,
+    agility: 25,
+    intelligence: 11
   },
-  fraction : 'water'
+  fraction: 'water'
 }
-let list = {Pickachu, Bulbasaur}
-var pokemonList = new PokemonList(list);
-var Charmander = {name: 'Charmander'};
-pokemonList.add(Charmander, 14, [21, 17,76], 'fire');
-var pokemon = new Pokemon(Charmander);
-pokemon.levelUp = true;
-pokemon.show('Charmander');
-pokemonList.showFraction('fire');
+let list = {
+  Pickachu,
+  Bulbasaur
+}
+var array = new Array();
+var pokemonList = new PokemonList(Pickachu, Bulbasaur);
+pokemonList.add('Charmander', 14, [21, 17, 76], 'fire');
+pokemonList.showList();
+// var pokemon = new Pokemon(Charmander);
+// pokemon.levelUp = true;
+// pokemon.show('Charmander');
+// pokemonList.showFraction('fire');

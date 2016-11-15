bd32f44edd0a968edf6a561f1162df46d68cce79
taranse <bboy.stenli@yandex.ru>
2016-11-16 01:13:32 +0300

    LEsson 4

--- LESSON 4 Stream/CReadable.js
+++ LESSON 4 Stream/CReadable.js
@@ -3,6 +3,8 @@
 //----------------------------------------------------
 
 const Readable  = require("stream").Readable;
+const Writable  = require("stream").Writable;
+const Transform   = require("stream").Transform;
 
 //--------------)>
 
@@ -11,29 +13,42 @@ class CReadable extends Readable {
     super(options);
   }
 
-  _read(size) {
-    this.push(size.toString());
-    this.push(null);
+  _read() {
+    function gen() {
+      const idx = Math.floor(Math.random( ) * (20+1));
+      return idx;
+    }
+    this.push(gen().toString());
   }
 }
 
+class CWritable extends Writable {
+  constructor(options) {
+    super(options);
+  }
+
+  _write(chunk, encoding, done) {
+    console.log(chunk.toString());
+    done();
+  }
+}
+
+class CLimit extends Transform {
+  constructor(max, options) {
+    super(options);
+    this.letensy1 = typeof max != "undefined" ? max : 1000;
+  }
+
+  _transform(chunk, encoding, done) {
+      setInterval(() => {
+        this.push('transform - ' + chunk.toString())
+      }, this.letensy1)
+  }
+}
 //--------------)>
 
 const read = new CReadable();
-
-read.on("data", console.log); // <-- (A)
-read.on("readable", function() {
-  let data;
-
-  while(data = read.read()) { // <-- (A): emit "data"
-    console.log(data.toString());
-  }
-});
-
-read.pause(); // <-- (A)
-
-// > node test.js
-//
-// readable
-// <Buffer 31 36 33 38 34>
-// "16384"
+const write = new CWritable();
+const tr = new CLimit(1000);
+read.pipe(write);
+read.pipe(tr).pipe(write);

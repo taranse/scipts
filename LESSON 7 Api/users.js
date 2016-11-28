e0872225cdc0a9ae0ec00d64db95ca1281ee3e2e
taranse <bboy.stenli@yandex.ru>
2016-11-29 00:03:49 +0300

    home

--- LESSON 7 Api/users.js
+++ LESSON 7 Api/users.js
@@ -13,15 +13,23 @@ module.exports = class Users extends Array{
         }else return `Такой пользователь уже существует`;
     }
     read(arg){
-        var newMass;
+        var newMass, fields = [], obj = {};
         arg.offset = arg.offset ? arg.offset : 0;//Если указан параметр offset
         arg.limit = arg.limit ? (arg.limit * 1 + arg.offset * 1) : Infinity;//Если указан параметр Limit
+        if(arg.fields) {
+            fields = arg.fields.split(',');
+        } //Если указан параметр fields [score, name]
         arg.name ? //Есть ли параметр имя
             newMass = this.filter((value, number) => value.name == arg.name && number >= arg.offset && number < arg.limit)
             : arg.score ? //Есть ли параметр score
                 newMass = this.filter((value, number) => value.score == arg.score && number >= arg.offset && number < arg.limit)
                 : arg.id ? //Есть ли параметр id
                     newMass = this[arg.id] : newMass = this.filter((value, number) => number >= arg.offset && number < arg.limit);
+        fields.length ?
+            newMass =  newMass.map((value, number) => {
+                for(var i of fields) obj[i] = value[i];
+                return obj;
+            }) : newMass = newMass;
         return newMass;
     }
     update(arg){
@@ -39,5 +47,8 @@ module.exports = class Users extends Array{
                     : newMass = 'Объекта с данным значением score нету в базе данных'
                 : newMass = 'Не указан параметр запроса';
         return newMass;
+    }
+    deleate(arg){
+
     }
 }

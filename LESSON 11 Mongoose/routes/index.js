3e98a2a32ac73825a76b8aa5a052668c8637f5d8
taranse <bboy.stenli@yandex.ru>
2016-12-16 01:40:46 +0300

    lesson 11

--- LESSON 11 Mongoose/routes/index.js
+++ LESSON 11 Mongoose/routes/index.js
@@ -2,29 +2,39 @@ const express = require('express');
 const router = express.Router();
 const User = require('models/user').User;
 const Task = require('models/task').Task;
-
+const obj = {};
+let getUsers = new Promise((resolve, reject) => {
+    User.find({}, function(err, users) {
+        resolve(users);
+    })
+})
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    User.find({}, (err, users) => users)
-        .then((users) => {
-            return new Promise(function(resolve, reject) {
-                User.findById(req.session.user, function (err, user) {
-                    if(err) reject(err);
-                    if(!user) resolve([user, users]);
-                    else res.redirect('/login');
-                })
-            })
+    getUsers
+        .then(users => {
+            User.findById(req.session.user, function (err, user) {
+                if(err) reject(err);
+                if(!user) {
+                    obj.user = user;
+                    obj.users = users;
+                }
+                else res.redirect('/login');
+            });
+            return obj;
         })
-        .then(function(resolve) {
-            Task.find({}, function(err, task) {
-                return task;
-            })
+        .then(obj => {
+            Task.find({}, function (err, task) {
+                if(err) throw err;
+                obj.task = task;
+            });
+            return obj;
         })
-        .then(function(resolve) {
-            res.send(resolve)
+        .then(obj => {
+            res.render('index', {resolve: obj});
         })
-        .catch(function(err) {next(err)});
-
+        .catch(function (resolve) {
+            next(resolve);
+        })
 });
 
 module.exports = router;

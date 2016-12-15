87610dcb2a26389b5322059a1ab887ee19f6caa5
taranse <bboy.stenli@yandex.ru>
2016-12-15 19:43:50 +0300

    lesson 11

--- LESSON 11 Mongoose/routes/index.js
+++ LESSON 11 Mongoose/routes/index.js
@@ -4,11 +4,15 @@ const User = require('models/user').User;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  User.findById(req.session.user, function (err, user) {
-      res.render('index', {
-          userId: user
-      })
-  })
+    User.find({}, (err, users) => users)
+        .then((users) => {
+            User.findById(req.session.user, function (err, user) {
+                if(!user) res.render('index', {user: user, users: users});
+                else res.redirect('/login');
+            })
+        })
+        .catch(function(err) {next(err)});
+
 });
 
 module.exports = router;

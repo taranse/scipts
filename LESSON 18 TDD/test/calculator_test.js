f66b64df9b062bbbd2a26d29d4969b22a7292e52
taranse <bboy.stenli@yandex.ru>
2017-01-17 13:21:41 +0300

    refactor

--- LESSON 18 TDD/test/calculator_test.js
+++ LESSON 18 TDD/test/calculator_test.js
@@ -2,63 +2,44 @@
 let assert = require('assert');
 let Calculator = require('../Class/calculator.js');
 
-function createCalculator() {
-    return new Calculator();
-}
+
 
 describe('string calculator factorial', function () {
+    var calculator = {};
+    beforeEach(function () {
+        calculator = new Calculator();
+    });
     it('should return 0 if fist param is empty', function(){
-        let calculator = createCalculator();
-
         let factorial = calculator.number('');
-
         assert.equal(factorial, 0);
     });
     it('should return 1 if first param is 0', function () {
-        let calculator = createCalculator();
-
         let factorial = calculator.number(0);
-
         assert.equal(factorial, 1);
 
     });
     it('should return 2 factorial if first param is 2', function () {
-        let calculator = createCalculator();
-
         let factorial = calculator.number(2);
-
         assert.equal(factorial, 2);
 
     });
     it('should return 6 factorial if first param is 3', function () {
-        let calculator = createCalculator();
-
         let factorial = calculator.number(3);
-
         assert.equal(factorial, 1*2*3);
 
     });
     it('should return 1*2*3*4*5*6*7*8*9*10 factorial if first param is 10', function () {
-        let calculator = createCalculator();
-
         let factorial = calculator.number(10);
-
         assert.equal(factorial, 1*2*3*4*5*6*7*8*9*10 );
 
     });
     it('should return 1*2*3*4*5*__*7*8*9*10 factorial if first param is 10 and second param is 6', function () {
-        let calculator = createCalculator();
-
         let factorial = calculator.number(10, 6);
-
         assert.equal(factorial, 1*2*3*4*5*7*8*9*10 );
 
     });
     it('should return 1*2*3*4*5*6*7*__*9*10 factorial if first param is 10 and second param is 6', function () {
-        let calculator = createCalculator();
-
         let factorial = calculator.number(10, 8);
-
         assert.equal(factorial, 1*2*3*4*5*6*7*9*10 );
 
     });

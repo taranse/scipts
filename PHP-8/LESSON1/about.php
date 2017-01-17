6a46f9e19aa3dfed6476086528cc3ec6c1cd7504
taranse <bboy.stenli@yandex.ru>
2017-01-17 15:41:02 +0300

    lesson 1

---
+++ PHP-8/LESSON1/about.php
@@ -0,0 +1,15 @@
+<?php
+function getAge($y, $m, $d) {
+    if($m > date('m') || $m == date('m') && $d > date('d')){
+        return (date('Y') - $y - 1);
+        
+    }
+    else{
+        return (date('Y') - $y);  
+    }
+}
+$name = 'Евгений';
+$age = getAge(1995, 1, 2);
+$email = 'bboy.stenli@yandex.ru';
+$city = 'Москва';
+$description = 'Ведущий программист в компании БиСиДжи';
---
+++ PHP-8/LESSON1/index.php
@@ -0,0 +1,29 @@
+<?php include 'about.php'; ?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title><?=$name?> - <?=$description?></title>
+</head>
+<style>
+    body{font-size: 20px;}
+    h1{text-align: center;}
+    table{margin: auto;}
+    table tr:nth-child(even){background-color: #ccc;}
+    td{padding: 10px;}
+</style>
+<body>
+    <h1>Страница пользователя <?=$name?></h1>
+    <table>
+        <tbody>
+            <tr><td>Имя</td><td><?=$name?></td></tr>
+            <tr><td>Возраст</td><td><?=$age?></td></tr>
+            <tr><td>Адрес электронной почты</td><td><a href="mailto:<?=$email?>"><?=$email?></a></td></tr>
+            <tr><td>Город</td><td><?=$city?></td></tr>
+            <tr><td>О себе</td><td><?=$description?></td></tr>
+        </tbody>
+    </table>
+</body>
+</html>
+

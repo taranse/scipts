49e0dc70175dc390641feeecb34c990e00096a5d
Evgeny <bboy.stenli@yandex.ru>
2017-04-01 12:03:43 +0300

    work-8

--- PHP-8/work-8/checkTest.php
+++ PHP-8/work-8/checkTest.php
@@ -1,9 +1,11 @@
 <?php
+if(empty($_POST['json'])) {
+	header('location: http://university.netology.ru/user_data/plyakin/work-8/list.php');
+}
+
 session_start();
 $_SESSION['count_success'] = !empty($_SESSION['count_success']) ? $_SESSION['count_success'] : 0;
 $test = file_get_contents('http://university.netology.ru/user_data/plyakin/work-8/tests/test-' . $_POST['json'] . '.json');
-
-
 $err = false;
 $test = json_decode($test, true);
 foreach($test as $key => $question) {
@@ -19,7 +21,7 @@ if ($err) {
     $_SESSION['count_success']++;
    echo ' ПОЗДРАВЛЯЕМ, ТЕСТ УСПЕШНО ПРОЙДЕН ' ;
 ?><br>
-<img src="http://irstorm.16mb.com/resource/captcha.php?name=<?= $_POST['name']; ?>" alt="">
+<img src="http://irstorm.16mb.com/resource/captcha.php?name=<?= $_POST['name']; ?>&count=<?= $_SESSION['count_success'] ?>" alt="">
 <?php } ?>
 <br><br>
-<a href="http://university.netology.ru/user_data/plyakin/work-7/list.php">Список тестов</a>
+<a href="http://university.netology.ru/user_data/plyakin/work-8/list.php">Список тестов</a>
--- PHP-8/work-8/createResult.php
+++ PHP-8/work-8/createResult.php
@@ -1,29 +1,12 @@
 <?php
-//session_start();
-//$code = '';
-//for($i=0;$i<5;$i++)
-//    $code .= chr(mt_rand(0,1) ? mt_rand(48, 57) : mt_rand(65, 90));
-//$Random = $code;
-//$_SESSION['captcha'] = md5($Random);
-//$img = imagecreatetruecolor(130, 40);
-//imagefilledrectangle($img, 0, 0, 130, 40, imagecolorallocate($img, 48, 24, 73));
-//imagettftext($img, 25, 0, 10, 30, imagecolorallocate($img, 255, 255, 0), 'font.otf', $Random);
-//
-//header('Expires: Wed, 1 Jan 1997 00:00:00 GMT');
-//header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
-//header('Cache-Control: no-store, no-cache, must-revalidate');
-//header('Cache-Control: post-check=0, pre-check=0', false);
-//header('Pragma: no-cache');
-//header ('Content-type: image/gif');
-//imagegif($img);
-//imagedestroy($img);
+session_start();
 
 $img = imagecreatetruecolor(500, 300);
 imagefilledrectangle($img, 0, 0, 0, 0, imagecolorallocate($img, 255, 255, 255));
 imagettftext($img, 30, 0, 10, 30, imagecolorallocate($img, 255, 255, 0), 'font.otf', '&#1044;&#1080;&#1087;&#1083;&#1086;&#1084;');
 imagettftext($img, 26, 0, 10, 100, imagecolorallocate($img, 255, 255, 0), 'font.otf', 'Javascript &#1088;&#1072;&#1079;&#1088;&#1072;&#1073;&#1086;&#1090;&#1095;&#1080;&#1082;');
 imagettftext($img, 30, 0, 10, 170, imagecolorallocate($img, 255, 255, 0), 'font.otf', $_GET['name']);
-imagettftext($img, 30, 0, 10, 240, imagecolorallocate($img, 255, 255, 0), 'font.otf', '&#1054;&#1094;&#1077;&#1085;&#1082;&#1072; - 5');
+imagettftext($img, 30, 0, 10, 240, imagecolorallocate($img, 255, 255, 0), 'font.otf', '&#1054;&#1094;&#1077;&#1085;&#1082;&#1072; - ' . $_SESSION['count_success']);
 
 header('Expires: Wed, 1 Jan 1997 00:00:00 GMT');
 header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');

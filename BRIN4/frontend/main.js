746afc4cc6a31115545eead0e9229d928830272b
taranse <bboy.stenli@yandex.ru>
2017-05-04 17:53:24 +0300

    brin

--- BRIN4/frontend/main.js
+++ BRIN4/frontend/main.js
@@ -1,3 +1,8 @@
 /**
  * Created by Evgeniy on 03.05.2017.
  */
+$('#video').find('img').click(function () {
+    let iframe = document.querySelector('.video-frame iframe');
+    iframe.src = 'https://www.youtube.com/embed/uzQKNBf3v_Q?autoplay=1';
+    $(this).css('display', 'none');
+});
--- BRIN4/frontend/materialize.js
+++ BRIN4/frontend/materialize.js
@@ -1,3 +1,5 @@
 /**
  * Created by Evgeniy on 03.05.2017.
  */
+window.$ = window.jQuery = require('../node_modules/jquery/dist/jquery.js'),
+    require('materialize-css');

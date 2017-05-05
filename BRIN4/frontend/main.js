9ad4c0f2eafa6a277afa821dac65895a31d84243
taranse <bboy.stenli@yandex.ru>
2017-05-05 15:51:03 +0300

    brin

--- BRIN4/frontend/main.js
+++ BRIN4/frontend/main.js
@@ -6,3 +6,11 @@ $('#video').find('img').click(function () {
     iframe.src = 'https://www.youtube.com/embed/uzQKNBf3v_Q?autoplay=1';
     $(this).css('display', 'none');
 });
+$('.more-reviews').click(function () {
+    $('.hide-reviews').animate({'height': 'toggle'}, 1000);
+});
+$('.more-text-toggle').click(function () {
+   $(this.parentNode.parentNode).find('div.more-text').animate({'height': 'toggle'}, 500);
+});
+
+$('.modal').modal();
